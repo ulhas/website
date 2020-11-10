a2e1ad08cc0b71c7f9e5aedb5d8d06149fb705ee
Ulhas Mandrawadkar <ulhas.sm@gmail.com>
2020-11-10 23:11:17 +0530

    Adds hero-text; links; removes hero-image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,13 @@
 import React from "react"
 
-import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeroSection from "../components/hero-section"
-import Navigation from "../components/navigation"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Ulhas | Home" />
-    <Navigation />
+  <div className="flex h-screen font-sans">
+    <SEO title="Home" />
     <HeroSection />
-  </Layout>
+  </div>
 )
 
 export default IndexPage
