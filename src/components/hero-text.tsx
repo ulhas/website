d774ff5a7424a07475cb65c2362e7f678c28456f
Ulhas Mandrawadkar <ulhas.sm@gmail.com>
2020-11-11 11:40:54 +0530

    Changes links default color

--- src/components/hero-text.tsx
+++ src/components/hero-text.tsx
@@ -5,9 +5,11 @@ import {IntrinsicProps} from '../shared/classes'
 const HeroText = ({className}: IntrinsicProps) => (
   <div className={className}>
     <h1 className="text-5xl font-extrabold tracking-tighter">
-      ULHAS
+      <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+        ULHAS
+      </span>
     </h1>
-    <p className="text-gray-700 text-lg font-light leading-loose">I am a <strong className="font-extrabold">software engineer and a mentor</strong>. Currently, I work as CTO at
+    <p className="text-gray-700 text-lg font-light leading-loose"><strong className="font-extrabold">I am a software engineer and a mentor</strong>. Currently, I work as CTO at
       <a href="https://procedure.tech" className="text-blue-500 hover:text-blue-700"> Procedure</a>, a company which I co-founded.
       Previously, I have been an early stage engineer in multiple startups including
       <a href="https://www.hypertrack.com" className="text-blue-500 hover:text-blue-700"> HyperTrack </a>
--- src/components/links.tsx
+++ src/components/links.tsx
@@ -6,16 +6,16 @@ import {IntrinsicProps} from '../shared/classes'
 
 const Links = ({className}: IntrinsicProps) => (
   <ul className={`${className} text-2xl font-extrabold`}>
-    <li className="lg:inline px-6 text-gray-700 hover:text-gray-900">
+    <li className="lg:inline lg:px-6 text-gray-700 hover:text-gray-900">
       <Link to="/about">ABOUT</Link>
     </li>
-    <li className="lg:inline px-6 text-red-500 hover:text-red-800">
+    <li className="lg:inline lg:px-6 text-gray-500 hover:text-red-800">
       <a href="https://bit.ly/rbrdk">YOUTUBE</a>
     </li>
-    <li className="lg:inline px-6 text-blue-500 hover:text-blue-800">
+    <li className="lg:inline lg:px-6 text-gray-500 hover:text-blue-800">
       <a href="https://twitter.com/ulhas_sm">TWITTER</a>
     </li>
-    <li className="lg:inline px-6 text-teal-500 hover:text-teal-800">
+    <li className="lg:inline lg:px-6 text-gray-500 hover:text-teal-800">
       <a href="https://linkedin.com/in/ulhasm">LINKEDIN</a>
     </li>
   </ul>
